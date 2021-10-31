553f50fc3513007042d2692b97d30893234169bc
ybgirgin3 <ybgirgin3@gmail.com>
2021-10-31 23:59:06 +0300

    updated/deleted

--- install_apps.sh
+++ install_apps.sh
@@ -8,11 +8,14 @@
 # TERMINAL GOODIES w/o gui
 sudo apt install software-properties-common apt-transport-https wget
 sudo apt remove vim           # remove old vim
-sudo apt install neovim       # neovim better than vim
+sudo apt install git
+sudo apt install zsh
 sudo apt install tmux         # best solution for spliting terminal screen                      
+sudo apt install curl
+sudo apt install neovim       # neovim better than vim
 sudo apt install subversion   # for cloning specific folder of repos
 
-
+# fun stuff
 sudo apt install screenfetch  # for info
 sudo apt install cmatrix      # for fun 🤟
 
@@ -28,13 +31,25 @@ sudo apt install geary                  # emails 📬
 sudo apt install obs-studio             # screen recording 💻
 
 
-# -- configuration needed apps
-# - vscode: 1st code editor
+# -- CONFIGURATION NEEDED APPS
+# -- NON-GUI APPS
+# --- gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+# --- touchegg: for trackpad shortcuts
+sudo add-apt-repository ppa:touchegg/stable
+sudo apt install touchegg     
+
+# -- GUI APPS
+# --- vscode: 1st code editor
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
 
-# - sublime text: 2nd code editor
+# --- sublime text: 2nd code editor
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -42,12 +57,15 @@ sudo apt-get update
 sudo apt-get install sublime-text
 
 
-# - spotify: for music
+# --- spotify: for music
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
 
-# - touchegg: for trackpad shortcuts
-sudo add-apt-repository ppa:touchegg/stable
-sudo apt install touchegg     
+# TERMINAL APPS RUN
+# -- oh my zsh run
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# -- github cli run/login
+gh login
