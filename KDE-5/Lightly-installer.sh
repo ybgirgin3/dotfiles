b0b8792d051dd899261272ebee47b95d610ae2e3
ybgirgin3 <ybgirgin3@gmail.com>
2021-11-06 20:23:53 +0300

    categorized

---
+++ KDE-5/Lightly-installer.sh
@@ -0,0 +1,26 @@
+# LIGHTLY INSTALLATION
+# clone the repo 
+gh repo clone Luwx/Lightly
+
+# download dependecies
+sudo apt install build-essential libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev extra-cmake-modules libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libkf5iconthemes-dev gettext qt3d5-dev -y
+
+cd Lightly && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
+make
+sudo make install
+
+
+# exist from lightly folder
+cd ../../
+
+
+# LIGHTY SHADER INSTALLATION
+gh repo clone Luwx/LightlyShaders
+sudo apt install git cmake g++ gettext extra-cmake-modules qttools5-dev libqt5x11extras5-dev libkf5configwidgets-dev libkf5crash-dev libkf5globalaccel-dev libkf5kio-dev libkf5notifications-dev kinit-dev kwin-dev -y
+
+cd LightlyShaders; mkdir qt5build; cd qt5build; cmake ../ -DCMAKE_INSTALL_PREFIX=/usr -DQT5BUILD=ON && make && sudo make install && (kwin_x11 --replace &)
+
+
+
+
