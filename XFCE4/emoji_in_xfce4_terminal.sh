9f22d4cf2bd2064ce05a0754f843b03ab32726e5
ybgirgin3 <ybgirgin3@gmail.com>
2021-12-31 09:57:01 +0300

    emoji support added to xfce terminal

---
+++ XFCE4/emoji_in_xfce4_terminal.sh
@@ -0,0 +1,13 @@
+# install the fonts
+cp -r emojione-android.ttf NotoColorEmoji.ttf ~/.local/share/fonts
+
+# run the command
+fc-cache -v -f
+
+echo "if you saw “fc-cache: succeeded” message it's done"
+
+# install emoji picker from snap
+sudo snap install emote
+
+echo "Do not forget to change Ctrl+Alt+e -> Super + ."
+
