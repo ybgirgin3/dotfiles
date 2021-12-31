0f6992b5b9949d11c80e8d27d72a0b9221f8f313
ybgirgin3 <ybgirgin3@gmail.com>
2021-12-31 10:39:48 +0300

    spotify yükleme şekli değiştirildi

--- spotify-installerr/spotify_installer.sh
+++ spotify-installerr/spotify_installer.sh
@@ -1,9 +1,12 @@
 # install spotify adblock
-gh repo clone abba23/spotify-adblock-linux
-cd spotify-adblock-linux/
-sudo apt install git wget tar make gcc libc-dev
-wget -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
-tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
-make
-sudo make install
+# gh repo clone abba23/spotify-adblock-linux
+# wget -O cef.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_88.1.6%2Bg4fe33a1%2Bchromium-88.0.4324.96_linux64_minimal.tar.bz2
+# tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
+# make
+# sudo make install
 
+sudo apt update
+sudo apt install git wget tar make gcc libc-dev cargo
+gh repo clone abba23/spotify-adblock
+cd spotify-adblock/
+make
