d97d6752bd50307825a4c18b76b724f94d5dff63
Yusuf Berkay Girgin <ybgirgin@armagroupholding.com>
2022-09-05 07:59:25 +0300

    vpn for job is added

--- Terminal/vpn/armavpn.sh
+++ Terminal/vpn/armavpn.sh
@@ -1,1 +1,1 @@
-sudo openfortivpn vpn.bul.com.tr:443 -u berkay.girgin --trusted-cert 14be56bdc7ec626281e6e9a387165e10117cb2016b9d22271dbcf02600aaa2ed --set-dns 0 --pppd-use-peerdns 1
+sudo openfortivpn <vpn-here> -u <name-here> --trusted-cert <certificate-here> --set-dns 0 --pppd-use-peerdns 1
