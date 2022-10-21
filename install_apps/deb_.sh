fd167a84bdd0bb0298164c83d0bc6157e55dc9f3
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-10-21 16:17:39 +0300

    ubuntu aur added

--- install_apps/deb_.sh
+++ install_apps/deb_.sh
@@ -4,6 +4,11 @@
 # Ubuntu
 ## Yusuf Berkay Girgin 31 October 2021 23:48
 
+# aur for ubuntu
+sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+
+# brew install 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # *** TERMINAL GOODIES w/o gui
 sudo apt install software-properties-common apt-transport-https wget -y
@@ -12,16 +17,13 @@ sudo apt install git -y
 sudo apt install zsh -y
 sudo apt install tmux -y         # best solution for spliting terminal screen                      
 sudo apt install curl -y
-sudo apt install neovim -y       # neovim better than vim
+# sudo apt install neovim -y       # neovim better than vim
 sudo apt install subversion -y   # for cloning specific folder of repos
 
-# google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-
-# fun stuff
-sudo apt install screenfetch -y  # for info
-sudo apt install cmatrix -y      # for fun 🤟
+pacstall -I discord
+pacstall -I notion-app-deb
+pacstall -I ripgrep
+pacstall -I lazygit
 
 ### APPLICATIONS ###
 # -- no configuration needed apps w/ gui
@@ -33,74 +35,50 @@ sudo apt install sqlitebrowser -y         # for sql dbs
 # sudo apt install geary -y                  # emails 📬 # no need in kde
 sudo apt install obs-studio -y             # screen recording 💻
 
-
-# -- CONFIGURATION NEEDED APPS
-# -- NON-GUI APPS
-# --- gh
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update -y
-sudo apt install gh -y
-
-# --- touchegg: for trackpad shortcuts
-#sudo add-apt-repository ppa:touchegg/stable -y
-#sudo apt install touchegg -y
-
 # --- fnm: light node version manager
 curl -fsSL https://fnm.vercel.app/install | bash
 
-
-
 # -- GUI APPS
+# --- google
+pacstall -I google-chrome-deb
+
 # --- vscode: 1st code editor
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code -y
+pacstall -I vscode-deb
 
 # --- sublime text: 2nd code editor
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update -y
-sudo apt-get install sublime-text -y
-
-# --- spotify: for music
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update -y && sudo apt-get install spotify-client -y
+pacstall -I sublime-text-deb
 
 # --- songrec: shazam client for linux
 sudo apt-add-repository ppa:marin-m/songrec -y -u
 sudo apt install songrec -y
 
-# *** TERMINAL APPS RUN
-
-# -- install astrovim
-mkdir -p  ~/.config/nvim/
-git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
-nvim +PackerSync
-
-
 # -- github cli run/login -> better with manually
+pacstall -I github-cli-deb
 
-# github configuration
-git config --global user.email "ybgirgin3@gmail.com"
-git config --global user.name "ybgirgin3"
+# install ohmyzsh and needed extensions
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 echo "Manual Needed Apps: Discord, Zoom, Mega.nz, xampp, miniconda for python 3.9"
 echo """
 manuel olarak çalıştırılması gereken komutlar:
 
-  # -- oh my zsh run
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
   # zsh extention
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+  # brew zsh configuration
+  # add these lines to .zshrc
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-  fnm install <lts-version-node>
-  gh auth login
-
-  !!!! GO TO http://neovimcraft.com/plugin/kabinspace/AstroVim/index.html to completely configure Astrovim
+  # most used alias
+  alias yzt="ssh -Y <ip>"
+  alias paradir="cd /home/berkay/Documents/"
+  alias vim="/home/berkay/.local/bin/lvim"
+  alias p="python3"
 """
