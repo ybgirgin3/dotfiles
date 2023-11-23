9bd83988e76e31eab1e83c33c20fdacffeab91f4
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2023-11-23 14:28:16 +0300

    only config not the repo

--- Terminal/lvim/config.lua
+++ Terminal/lvim/config.lua
@@ -10,34 +10,25 @@ vim.opt.cursorline = false
 
 
 -- general
-lvim.log.level = "info"
-lvim.format_on_save = {
-	enabled = true,
-	pattern = "*.lua",
-	timeout = 1000,
-}
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
-
 lvim.transparent_window = false
 
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<D-s>"] = ":w<cr>"
 
+-- Disable Line Moving
+lvim.keys.normal_mode["<A-j>"] = ""
+lvim.keys.normal_mode["<A-k>"] = ""
 
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.visual_block_mode["<A-j>"] = ""
+lvim.keys.visual_block_mode["<A-k>"] = ""
 
--- -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.keys.insert_mode["<A-j>"] = ""
+lvim.keys.insert_mode["<A-k>"] = ""
 
 -- -- Change theme settings
--- lvim.colorscheme = "lunar"
 lvim.colorscheme = 'peachpuff'
--- lvim.colorscheme = 'peachpuff-dark'
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -48,37 +39,8 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
 
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
-
--- -- always installed on startup, useful for parsers without a strict filetype
--- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
-
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
 -- --- disable automatic installation of servers
 lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
 
 -- -- linters, formatters and code actions <https://www.lunarvim.org/docs/languages#lintingformatting>
 local formatters = require("lvim.lsp.null-ls.formatters")
@@ -89,6 +51,7 @@ formatters.setup({
 		extra_args = { "--print-width", "100" },
 		filetypes = { "typescript", "typescriptreact" },
 	},
+    { name = 'black' }
 })
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
@@ -107,14 +70,6 @@ code_actions.setup({
 	},
 })
 
--- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
-lvim.plugins = {
-	{
-		"folke/trouble.nvim",
-		cmd = "TroubleToggle",
-	},
-}
-
 -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 lvim.autocommands = {
   {
@@ -132,16 +87,8 @@ lvim.autocommands = {
     },
   },
 }
--- ctermfg=White ctermbg=Yellow cterm=bold
 
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
-
+-- DEBUGGING
 -- install plugins
 lvim.plugins = {
   "ChristianChiarulli/swenv.nvim",
@@ -159,12 +106,14 @@ lvim.builtin.treesitter.ensure_installed = {
 -- setup formatting
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { name = "black" }, }
-lvim.format_on_save.enabled = true
-lvim.format_on_save.pattern = "*.py"
 
--- setup linting
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { command = "flake8", filetypes = { "python" } } }
+lvim.log.level = "info"
+lvim.format_on_save = {
+	enabled = true,
+	pattern = "*.py",
+	timeout = 1000,
+}
+
 
 -- setup debug adapter
 lvim.builtin.dap.active = true
