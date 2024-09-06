57df9341d36fe563ac3b567f85ccb3f956db78fa
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-09-06 15:27:33 +0300

    wezterm config

--- Terminal/.wezterm.lua
+++ Terminal/.wezterm.lua
@@ -1,20 +1,88 @@
--- WezTerm konfigürasyon dosyası
+local wezterm = require("wezterm")
+local config = {}
 
-local wezterm = require 'wezterm'
-
-return {
-  -- Font ayarları
-  -- font = wezterm.font("Liga SFMono Nerd Font"),
-  -- font = wezterm.font("Hack Nerd Font Mono"),
-  font = wezterm.font("MesloLGM Nerd Font Mono"),
-  font_size = 12.0, -- Burada font boyutunu ihtiyacınıza göre ayarlayabilirsiniz
-
-  -- Şeffaflık ayarları
-  enable_wayland = false, -- Eğer Wayland kullanıyorsanız bunu true yapın
-  window_background_opacity = 0.8, -- Pencerede arka plan şeffaflığını ayarlar. 1.0 tamamen opak, 0.0 tamamen şeffaf
-
-  -- Arka plan bulanıklaştırma ayarları
-  -- use_fancy_tab_bar = false, -- Eğer tab çubuğunda bulanıklık istiyorsanız bunu true yapın
-  -- enable_ime = true, -- Input method editor (IME) kullanımını etkinleştirir
+config = {
+  default_cursor_style = "BlinkingBar",
+  automatically_reload_config = true,
+  window_close_confirmation = "NeverPrompt",
+  adjust_window_size_when_changing_font_size = false,
+  window_decorations = "RESIZE",
+  check_for_updates = false,
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = false,
+  font_size = 12.5,
+  font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+  enable_tab_bar = true,
+  window_padding = {
+    left = 3,
+    right = 3,
+    top = 0,
+    bottom = 0,
+  },
+  background = {
+    {
+      source = {
+        File = "/Users/yusufberkaygirgin/Pictures/terminal_background.jpg",
+      },
+      hsb = {
+        hue = 1.0,
+        saturation = 1.02,
+        brightness = 0.25,
+      },
+      width = "100%",
+      height = "100%",
+    },
+    {
+      source = {
+        Color = "#282c35",
+      },
+      width = "100%",
+      height = "100%",
+      opacity = 0.55,
+    },
+  },
+  -- from: https://akos.ma/blog/adopting-wezterm/
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = "\\((\\w+://\\S+)\\)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = "\\[(\\w+://\\S+)\\]",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = "\\{(\\w+://\\S+)\\}",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = "<(\\w+://\\S+)>",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      -- Before
+      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      --format = '$0',
+      -- After
+      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- implicit mailto link
+    {
+      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+      format = "mailto:$0",
+    },
+  },
 }
+return config
 
